d8bad3698a61e1a278b5cc624e2bc11999283606
billy <billydev@gmail.com>
2020-08-02 13:32:44 -0600

    commit ignore file with new changes

--- 11021Exp020/src/main/java/billydev/App.java
+++ 11021Exp020/src/main/java/billydev/App.java
@@ -2,6 +2,8 @@ package billydev;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println();
+        System.out.println();
+        //this is local added
     }
 }
